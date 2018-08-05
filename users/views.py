83a654dc46b5145bbc6a9a38d11a6227913c9f0c
SheriffHood <h77max@163.com>
2018-08-05 15:33:05 +0800

    build login page

--- users/views.py
+++ users/views.py
@@ -7,25 +7,18 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.views.generic.base import View
 from django.views.generic import TemplateView
-from django.db.models import Q
 
-class LoginView(TemplateView):
-    def post(self, request, **kwargs):
-        user = authenticate(username=user_name, password=pass_word)
+def user_login(request):
+    if request.method == "POST":
+        user_name = request.POST.get("username", "")
+        pass_word = request.POST.get("password", "")
+
+        user = authenticate(username= user_name, password= pass_word)
 
         if user is not None:
             login(request, user)
-            return render(request, 'index.html')
+            return render(request, "index.html")
+      
         else:
-            return render(request, 'login.html', {'msg':'wrong pwd or username'})
+            return render(request, "login.html", {})
 
-
-class CustomBackend(ModelBackend):
-    def authenticate(self, username=None, password=None, **kwarg):
-        try:
-            user = UserProfile.objects.get( Q(username=username)  | Q(email=username) )
-            if user.check_password(password):
-                return user
-
-        except Exception as e:
-            return None
