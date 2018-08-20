3084e64aba01de270fd8cf6653758aaead6484e4
SheriffHood <h77max@163.com>
2018-08-21 01:41:23 +0800

    build filter

--- organization/views.py
+++ organization/views.py
@@ -13,7 +13,11 @@ class OrgView(View):
     def get(self, request):
         all_orgs = CourseOrg.objects.all()
         org_nums = all_orgs.count()
+
         all_citys = CityDict.objects.all()
+        city_id = request.GET.get('all_citys', "")
+        if city_id:
+            all_orgs = all_orgs.filter(city_id(int(city_id)))
 
         try:
             page = request.GET.get('page', 1)
@@ -27,4 +31,5 @@ class OrgView(View):
             "all_orgs":orgs,
             "all_citys":all_citys,
             "org_nums":org_nums,
+            "city_id":city_id,
         })
