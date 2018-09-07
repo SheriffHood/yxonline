f6dea0742e7c4079bcfc98e7079becf0fe319117
SheriffHood <h77max@163.com>
2018-09-08 05:48:33 +0800

    fixing url bug

--- organization/urls.py
+++ organization/urls.py
@@ -7,7 +7,7 @@ from django.urls import path, re_path
 app_name = 'organization'
 
 urlpatterns = [
-    path('list/', OrgView.as_view(), name='org_list'),
+    path('list/', OrgView.as_view(), name='list'),
     path('add_ask/', AddUserAskView.as_view(), name='add_ask'),
     re_path('home/(?P<org_id>\d+)/', OrgHomeView.as_view(), name='org_home'),
 ]
