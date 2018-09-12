65c7d51782aaff93c835d20a2ded0bce9d308471
SheriffHood <h77max@163.com>
2018-09-12 21:47:47 +0800

    update

--- organization/urls.py
+++ organization/urls.py
@@ -13,6 +13,5 @@ urlpatterns = [
     re_path('course/(?P<org_id>\d+)/', OrgCourseView.as_view(), name='org_course'),
     re_path('desc/(?P<org_id>\d+)/', OrgDescView.as_view(), name='org_desc'),
     re_path('teacher/(?P<org_id>\d+)/', OrgTeacherView.as_view(), name='org_teacher'),
-
     path('add_fav/', AddFavView.as_view(), name='add_fav'),
 ]
