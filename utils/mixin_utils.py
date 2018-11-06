33142d34b46bc212d1ba96a732dc50b7ebc7746e
SheriffHood <h77max@163.com>
2018-11-07 06:44:52 +0800

    finish login required

--- utils/mixin_utils.py
+++ utils/mixin_utils.py
@@ -7,5 +7,5 @@ from django.utils.decorators import method_decorator
 class LoginRequiredMixin(object):
     
     @method_decorator(login_required(login_url='/login/'))
-    def difpatch(self, request, *args, **kwargs):
+    def dispatch(self, request, *args, **kwargs):
         return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
