31c3d0b5c226161c8a61a116b4a9b13b006b7376
SheriffHood <h77max@163.com>
2018-12-06 06:40:53 +0800

    bulid page link

--- courses/models.py
+++ courses/models.py
@@ -19,6 +19,7 @@ class Course(models.Model):
     name = models.CharField(max_length=50, verbose_name=u'课程名')
     desc = models.CharField(max_length=300, verbose_name=u'课程描述')
     detail = models.TextField(verbose_name=u'课程详情')
+    is_banner = models.BooleanField(default=False, verbose_name="是否轮播")
     degree = models.CharField(choices=DEGREE_CHOICES, max_length=2)
     learn_times = models.IntegerField(default=0, verbose_name=u'学习时长(分钟数)')
     students = models.IntegerField(default=0, verbose_name=u'学习人数')
